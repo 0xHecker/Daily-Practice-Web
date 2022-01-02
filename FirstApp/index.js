7d6d8b7dcea737e952f2e1fe6325ea53bf3d36c9
Sai Shanmukh <sdgg0227@gmail.com>
2022-01-02 23:50:27 +0530

    views, making partials

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -6,7 +6,9 @@ const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home.ejs');
