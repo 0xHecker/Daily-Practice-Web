360d549e0b9a709aeefd008486f0ab783d0066fa
xHecker <91829843+xHecker@users.noreply.github.com>
2022-01-25 15:20:48 +0530

    'this' keyword uses noted (imp)

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -3,19 +3,42 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = { 
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
     };
 
-     styles = {
+    styles = {
          fontSize: 10,
          fontWeight: 'bold'
      }
+
+    //  constructor(){
+    //      super();
+    //      this.handleIncrement = this.handleIncrement.bind(this);
+    //  }
+    // 4) we can use a constructor where we have access to 'this' and set value of 'this' for our function
+     
+    handleIncrement() {
+         console.log('Increment Clicked',this)
+     }
+
+     // if we call 'this' with a object function 
+     // 1) obj.method() ==> 'this' returns (obj)
+     // if called from a standalone function 
+     // 2) function(); ===> 'this' returns undefined if strict mode enabled or a window object 
+     // 3) this from an arrow function inherets properties from nearby function 
+
     render() {
         return (
         <div>
-            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
-            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+            <span 
+                style={this.styles} 
+                className={this.getBadgeClasses()} > 
+                {this.formatCount()}
+            </span>
+            <button 
+                onClick={this.handleIncrement} 
+                className='btn btn-secondary btn-sm'>
+                Increment
+            </button>
         </div>
         );
     }
